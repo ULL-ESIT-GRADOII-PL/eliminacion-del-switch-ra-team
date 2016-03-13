f678ced1e0ed62cb3582072ad66c63e956de6436
Adrián Rodríguez Bazaga <alu0100826456@ull.edu.es>
2016-03-13 20:34:47 +0000

    Code from celsius.js beautified

--- assets/js/celsius.js
+++ assets/js/celsius.js
@@ -1,30 +1,27 @@
 (function(exports) {
-
-
-
-  function Celsius(valor, type) {
-    converters = {}
-    converters['k'] = function (value) {
-      return value - 273.15;
+    function Celsius(valor, type) {
+        converters = {}
+        converters['k'] = function(value) {
+            return value - 273.15;
+        }
+        converters['c'] = function(value) {
+            return value;
+        }
+        converters['f'] = function(value) {
+            return (value - 32) * 5 / 9;
+        }
+        if (converters[type])
+            valor = converters[type](valor);
+        else {
+            valor = Medida.invalidConversion(type, 'Celsius');
+        }
+        Temperatura.call(this, valor, 'Celsius');
     }
-    converters['c'] = function (value) {
-      return value;
-    }
-    converters['f'] = function (value) {
-      return (value - 32) * 5 / 9;
-    }
-    if (converters[type])
-        valor = converters[type](valor);
-    else {
-        valor = Medida.invalidConversion (type, 'Celsius');
-    }
-      Temperatura.call(this, valor, 'Celsius');
-  }
 
-  // There we set the inheritance
-  Celsius.prototype = new Temperatura();
-  Celsius.prototype.constructor = Celsius;
+    // There we set the inheritance
+    Celsius.prototype = new Temperatura();
+    Celsius.prototype.constructor = Celsius;
 
-  exports.Celsius = Celsius;
+    exports.Celsius = Celsius;
 
 })(this);
