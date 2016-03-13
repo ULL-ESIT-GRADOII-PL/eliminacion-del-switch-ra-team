82800688a1b890e802a4ab4823bb9424338afea7
Adrián Rodríguez Bazaga <alu0100826456@ull.edu.es>
2016-03-13 20:36:35 +0000

    Code from longitud.js beautified

--- assets/js/longitud.js
+++ assets/js/longitud.js
@@ -1,11 +1,10 @@
-
 (function(exports) {
-  function Longitud(valor, tipo) {
-      Medida.call(this, valor, tipo);
-  }
+    function Longitud(valor, tipo) {
+        Medida.call(this, valor, tipo);
+    }
 
-  // There we set the inheritance
-  Longitud.prototype = new Medida("34c");
-  Longitud.prototype.constructor = Longitud;
-  exports.Longitud = Longitud
+    // There we set the inheritance
+    Longitud.prototype = new Medida("34c");
+    Longitud.prototype.constructor = Longitud;
+    exports.Longitud = Longitud
 })(this);
