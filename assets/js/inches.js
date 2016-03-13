11c760dec1e4a6ae0086e0befd43da08a826ccd0
Adrián Rodríguez Bazaga <alu0100826456@ull.edu.es>
2016-03-13 20:35:43 +0000

    Code from inches.js beautified

--- assets/js/inches.js
+++ assets/js/inches.js
@@ -1,24 +1,23 @@
-
 (function(exports) {
-      function Inches(valor, type) {
+    function Inches(valor, type) {
         converters = {}
-        converters['i'] = function (value) {
-          return value;
+        converters['i'] = function(value) {
+            return value;
         }
-        converters['m'] = function (value) {
-          return (value * 39.3701);
+        converters['m'] = function(value) {
+            return (value * 39.3701);
         }
         if (converters[type])
             valor = converters[type](valor);
         else {
-            valor = Medida.invalidConversion (type, 'Inches');
+            valor = Medida.invalidConversion(type, 'Inches');
         }
-          Longitud.call(this, valor, "Inches");
-      }
+        Longitud.call(this, valor, "Inches");
+    }
 
-      Inches.prototype = new Longitud();
-      Inches.prototype.constructor = Inches;
+    Inches.prototype = new Longitud();
+    Inches.prototype.constructor = Inches;
 
-      exports.Inches = Inches
+    exports.Inches = Inches
 
 })(this);
