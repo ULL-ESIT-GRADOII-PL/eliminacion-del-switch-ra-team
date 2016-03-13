aae0f610806c2194ef5c2c2a05871529155bc9b0
Adrián Rodríguez Bazaga <alu0100826456@ull.edu.es>
2016-03-13 20:36:15 +0000

    Code from kelvin.js beautified

--- assets/js/kelvin.js
+++ assets/js/kelvin.js
@@ -1,29 +1,27 @@
-
 (function(exports) {
-
-      function Kelvin(valor, type) {
+    function Kelvin(valor, type) {
         converters = {}
-        converters['k'] = function (value) {
-          return value;
+        converters['k'] = function(value) {
+            return value;
         }
-        converters['c'] = function (value) {
-          return (value + 273.15);
+        converters['c'] = function(value) {
+            return (value + 273.15);
         }
-        converters['f'] = function (value) {
-          return (5 * (value - 32) / 9) + 273.15;
+        converters['f'] = function(value) {
+            return (5 * (value - 32) / 9) + 273.15;
         }
         if (converters[type])
-          valor = converters[type](valor);
+            valor = converters[type](valor);
         else {
-          valor = Medida.invalidConversion (type, 'Kelvin');
+            valor = Medida.invalidConversion(type, 'Kelvin');
         }
         Temperatura.call(this, valor, 'Kelvin');
-      }
-      // There we set the inheritance
-      Kelvin.prototype = new Temperatura();
-      Kelvin.prototype.constructor = Kelvin;
+    }
+    // There we set the inheritance
+    Kelvin.prototype = new Temperatura();
+    Kelvin.prototype.constructor = Kelvin;
 
 
-      exports.Kelvin = Kelvin;
+    exports.Kelvin = Kelvin;
 
 })(this);
